fa513e28f7470075fefd1feb1ea992f0640cefc4
aferreiradominguez <aferreiradominguez@atanasoff07.danielcastelao.org>
2015-11-06 10:07:45 +0100

    funcionando

--- InterUser.py
+++ InterUser.py
@@ -4,27 +4,30 @@
 
 from gi.repository import Gtk
 
+
 class FiestraPrincipal:
+    def __init__(self):
+        ficheiro = "Inter.glade"
+        builder = Gtk.Builder()
+        builder.add_from_file(ficheiro)
 
-   def __init__(self):
-    ficheiro="Inter.glade"
-    builder=Gtk.Builder()
-    builder.add_from_file(ficheiro)
+        sinais = {"on_txtCadro_activate": self.on_txtCadro_activate,
+                  "on_btnAceptar_clicked": self.on_btnBoton_clicked,
+                  "on_fiestraPrincipal_destroy": Gtk.main_quit}
 
-    sinais={"on_txtCadro_activate":self.on_txtCadro_activado,"on_btnAceptar_clicked":self.on_btnAceptar_clicked,"on_fiestraPrincipal_destroy":Gtk.main_quit}
+        builder.connect_signals(sinais)
+        self.etiqueta = builder.get_object("lblEtiqueta")
+        self.cadroTexto = builder.get_object("txtCadro")
+        self.fiesra = builder.get_object("FiestraPrincipal")
 
-    builder.connect_signals(sinais)
-    self.etiqueta=builder.get_object("lblEtiqueta")
-    self.cadroTexto=builder.get_object("txtCadro")
-    self.fiesra=builder.get_object("FiestraPrincipal")
+    def on_btnBoton_clicked(self, control):
+        texto = self.cadroTexto.get_text()
+        self.etiqueta.set_text("ola %s" %texto)
 
-   def on_btnBoton_clicked(self,control):
-       texto=self.cadroTexto.get_text()
-       self.etiqueta.set_text("ola %s" %texto)
+    def on_txtCadro_activate(self, control):
+        self.on_btnBoton_clicked(control)
 
-   def on_txtCadro_activate(self,control):
-       self.on_btnBoton_clicked(control)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     FiestraPrincipal()
     Gtk.main()
